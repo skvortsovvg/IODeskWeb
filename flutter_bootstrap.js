c72e0b8d0b5a7a6614b85685d5f02f5491dc9930
skvortsovvg <vovanches@i.ua>
2024-10-24 12:27:38 +0300

    font correction login page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1857247945"
+    serviceWorkerVersion: "4165897975"
   }
 });
