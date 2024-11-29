d7fba5bd8f7cb21d279ae8bfcd9a80183baed987
skvortsovvg <vovanches@i.ua>
2024-11-29 15:30:28 +0200

    upd, v.1.8.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,13 +39,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "768bc080e69c4b2f94196a509f46b623
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "a7849c2da1f1ecf7341da25ec98bf394",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "2c3306268536b8fe6138893c244b1ccd",
+"flutter_bootstrap.js": "e0dba45d5c4562023202fee37e47b5c8",
 "icons/app_launcher_icon.jpg": "21f6ac7a831e27068114f42804741ebc",
 "icons/favicon.png": "a7849c2da1f1ecf7341da25ec98bf394",
 "index.html": "cfd8db2e6275074ede1eac9fe80c3087",
 "/": "cfd8db2e6275074ede1eac9fe80c3087",
-"main.dart.js": "96da6c5c019c97478c7cc34c0470a974",
-"version.json": "94724296460bae388858d450dccff99c"};
+"main.dart.js": "f4ec49f7295277120ed1a6d1d362e95f",
+"version.json": "f5c8321451a8ae97a2186559c1887bcb"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
